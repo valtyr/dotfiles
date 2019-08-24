a0449599c0f026b884be7830d2634be3a8a59054
Valtýr Örn Kjartansson <valtyr@gmail.com>
2019-08-24 19:06:22 +0000

    Update script

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
 set -euo pipefail
 
-apt update
-apt install fish neovim hub mosh
+sudo apt update
+sudo apt install fish neovim hub mosh
 
 chsh $USER -s $(which fish)
 
@@ -16,7 +16,7 @@ ln -fs ~/.dotfiles/init.vim ~/.config/nvim/init.vim
 ln -fs ~/.dotfiles/config.fish ~/.config/fish/config.fish
 ln -fs ~/.dotfiles/update.fish ~/.config/fish/functions/update-dotfiles.fish
 
-curl -sL install-node.now.sh/lts | bash
+# curl -sL install-node.now.sh/lts | sudo bash
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 nvim -E -c PlugInstall -c q
